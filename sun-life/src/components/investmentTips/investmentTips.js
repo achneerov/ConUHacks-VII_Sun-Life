a42e06bcc892aef0c596aea5b6939c0348cff93a
alexC223 <08_basalts.tonnage@icloud.com>
2023-01-21 21:37:01 -0500

    added points

--- sun-life/src/components/investmentTips/investmentTips.js
+++ sun-life/src/components/investmentTips/investmentTips.js
@@ -6,25 +6,39 @@ import './index.css';
 
 const investmentTips = props => (
 	<div>
-<title>Saving money during a crisis: do's and don'ts</title>
+
+<h1>Saving money during a crisis: do's and don'ts</h1>
+
+
+
 <title>Sun Life financial security advisor Julien Ringuette has the following recommendations.</title>
 
-<title>Do's</title>
+<h1>Do's</h1>
 
-<body>Make a plan, or review the plan you already have. It’s best to build your plan with your advisor’s help. And once you have a plan, you should stick to it! 
-Invest sooner rather than later, in RRSPs, TFSAs, stocks. The more you invest, the more opportunities you have to increase your buying power and combat the effects of inflation. 
-Automate your savings with automatic deposits. You’ll be able to set aside a fixed sum of money at a frequency you choose. Automating your savings takes the stress out of saving.
-Be patient. With time, things often sort themselves out.</body>
+<ul>
+  <li>Make a plan, or review the plan you already have. It’s best to build your plan with your advisor’s help. And once you have a plan, you should stick to it!</li>
+  <li>Invest sooner rather than later, in RRSPs, TFSAs, stocks.</li>
+  <li>The more you invest, the more opportunities you have to increase your buying power and combat the effects of inflation.</li>
+  <li>Automate your savings with automatic deposits. You’ll be able to set aside a fixed sum of money at a frequency you choose. Automating your savings takes the stress out of saving.</li>
+	<li>Be patient. With time, things often sort themselves out.</li>
+</ul>
 
-<title>Don’ts</title>
 
-<body>Make rash, impulsive decisions.
-Rely entirely and only on random internet pages for answers. 
-Make impulse RRSP withdrawals because you’re stressed. 
-Give in to panic and lose sight of your long-term vision. 
-Try to play the stock market if you don’t have experience buying and selling stocks. That’s like playing with fire!</body>
 
-		
+
+
+
+<h1>Don'ts</h1>
+<ul>
+  <li>Make rash, impulsive decisions.</li>
+  <li>Rely entirely and only on random internet pages for answers.</li>
+  <li>Make impulse RRSP withdrawals because you’re stressed.</li>
+  <li>Give in to panic and lose sight of your long-term vision.</li>
+	<li>Be patient. With time, things often sort themselves out.</li>
+	<li>Try to play the stock market if you don’t have experience buying and selling stocks. That’s like playing with fire!</li>
+</ul>
+
+
 	</div>
 );
 
