3c61b7ec34bf4960448a5fb2bed61afb8d64ee6d
Minh Anh Dao <minhanhdao0912@gmail.com>
2023-01-22 10:24:54 -0500

    clean up folders

--- sun-life/src/components/investmentTips/investmentTips.js
+++ sun-life/src/components/investmentTips/investmentTips.js
@@ -5,31 +5,30 @@ import './index.css';
 <link rel="stylesheet" type="text/css" href="index.css" />
 
 const investmentTips = props => (
-	<div>
-      <h1>Saving money during a crisis: do's and don'ts</h1>
+  <div>
+    <h1>Saving money during a crisis: do's and don'ts</h1>
 
-      <title>Sun Life financial security advisor Julien Ringuette has the following recommendations.</title>
+    <title>Sun Life financial security advisor Julien Ringuette has the following recommendations.</title>
 
-      <h1>Do's</h1>
+    <h1>Do's</h1>
 
-        <ul>
-          <li>Make a plan, or review the plan you already have. It’s best to build your plan with your advisor’s help. And once you have a plan, you should stick to it!</li>
-          <li>Invest sooner rather than later, in RRSPs, TFSAs, stocks.</li>
-          <li>The more you invest, the more opportunities you have to increase your buying power and combat the effects of inflation.</li>
-          <li>Automate your savings with automatic deposits. You’ll be able to set aside a fixed sum of money at a frequency you choose. Automating your savings takes the stress out of saving.</li>
-	        <li>Be patient. With time, things often sort themselves out.</li>
-        </ul>
+    <ul>
+      <li>Make a plan, or review the plan you already have. It’s best to build your plan with your advisor’s help. And once you have a plan, you should stick to it!</li>
+      <li>Invest sooner rather than later, in RRSPs, TFSAs, stocks.</li>
+      <li>The more you invest, the more opportunities you have to increase your buying power and combat the effects of inflation.</li>
+      <li>Automate your savings with automatic deposits. You’ll be able to set aside a fixed sum of money at a frequency you choose. Automating your savings takes the stress out of saving.</li>
+      <li>Be patient. With time, things often sort themselves out.</li>
+    </ul>
 
-      <h1>Don'ts</h1>
-        <ul>
-          <li>Make rash, impulsive decisions.</li>
-          <li>Rely entirely and only on random internet pages for answers.</li>
-          <li>Make impulse RRSP withdrawals because you’re stressed.</li>
-          <li>Give in to panic and lose sight of your long-term vision.</li>
-	        <li>Be patient. With time, things often sort themselves out.</li>
-	        <li>Try to play the stock market if you don’t have experience buying and selling stocks. That’s like playing with fire!</li>
-        </ul>
-	</div>
+    <h1>Don'ts</h1>
+    <ul>
+      <li>Make rash, impulsive decisions.</li>
+      <li>Rely entirely and only on random internet pages for answers.</li>
+      <li>Make impulse RRSP withdrawals because you’re stressed.</li>
+      <li>Give in to panic and lose sight of your long-term vision.</li>
+      <li>Try to play the stock market if you don’t have experience buying and selling stocks. That’s like playing with fire!</li>
+    </ul>
+  </div>
 );
 
 // todo: Unless you need to use lifecycle methods or local state,
@@ -42,7 +41,7 @@ const investmentTips = props => (
 // }
 
 const investmentTipsPropTypes = {
-	// always use prop types!
+  // always use prop types!
 };
 
 investmentTips.propTypes = investmentTipsPropTypes;
